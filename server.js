9192c0a96e133c78207a877e41719f41b50f1976
Max Kopych <max@cubicleninjas.com>
2018-11-19 15:41:04 -0600

    Simple Hard Coded

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const expressGrapgQL = require("express-graphql");
+const schema = require("./schema")
 
 const app = express();
+
+app.use("/graphql", expressGrapgQL({ //you can go to this url and check it http://localhost:4000/graphq
+  schema,
+  graphiql: true
+}));
 
 app.listen(4000, ()=>{
   console.log("Server running on port 4000")
