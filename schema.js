c7d5ff63613ce29149b554d76a5be074f622c3b2
Max Kopych <max@cubicleninjas.com>
2018-11-19 16:15:49 -0600

    Adding JSON Server

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const  {
   GraphQLObjectType,
   GraphQLString,
@@ -7,13 +8,13 @@ const  {
   GraphQLNonNull
 } = require("graphql");
 
-//HardCoded Data
+/*//HardCoded Data
 
 const customers = [
   {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:35},
   {id:'2', name:'Steve Smith', email:'steve@gmail.com', age:25},
   {id:'3', name:'Sara Williams', email:'sara@gmail.com', age:32},
-];
+];*/
 
 
 //Customer Type
@@ -37,17 +38,20 @@ const RootQuery = new GraphQLObjectType({
         id: {type:GraphQLString}
       },
       resolve(parentValue, args){
-        for(let i = 0; i < customers.length; i++){
+        /*for(let i = 0; i < customers.length; i++){
           if(customers[i].id == args.id){
             return customers[i];
           }
-        }
+        }*/
+        return axios.get("http://localhost:3000/customers/"+args.id)
+          .then( res => res.data )
       }
     },
     customers: {  //if no arg then without ()
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args){
-        return customers;
+        return axios.get("http://localhost:3000/customers/")
+          .then( res => res.data )
       }
     }
   }
